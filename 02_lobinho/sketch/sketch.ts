45208286491fbc9becf5bf0caf18aa9d61f82dcf
Antonio Guilherme Mendes Sampaio <gmendes.sam@gmail.com>
2021-10-07 16:15:28 -0300

    atualiazabd lobinho

---
+++ 02_lobinho/sketch/sketch.ts
@@ -0,0 +1,105 @@
+class Entity{
+    x: number;
+    y: number;
+    step: number;
+    image: p5.Image;
+
+    constructor(x: number, y: number, step: number, image: p5.Image) {
+        this.x = x;
+        this.y = y;
+        this.step = step;
+        this.image = image;
+    }
+
+    draw(){
+        image(this.image, this.x * this.step, this.y * this.step, this.step, this.step);
+    }
+}
+
+class Board{
+    nl: number;
+    nc: number;
+    step: number;
+    background: p5.Image;
+
+    constructor(nl: number, nc:number, step: number, background: p5.Image) {
+        this.nl = nl;
+        this.nc = nc;
+        this.step = step;
+        this.background = background;
+    }
+
+    draw(): void {
+        image(this.background, 0, 0, this.nc * this.step, this.nl * this.step)
+         for(let x = 0; x < this.nc; x++) {
+            for(let y = 0; y < this.nl; y++) {
+                noFill();
+                stroke(0);
+                strokeWeight(2);
+                rect(x * this.step, y * this.step, this.step, this.step);
+            }
+        }
+    }
+}
+
+let wolf_image: p5.image;
+let rabbit_image: p5.image;
+let board_image: p5.image;
+
+let wolf: Entity;
+let rabbit: Entity;
+let board: Board;
+
+function load(path: string): p5.Image {
+    return loadImage(
+        path, 
+        () => console.log('Loading' + path + 'ok'), 
+        () => console.log('Loading' + path + 'error')
+    );
+}
+
+function preload() {
+    wolf_image = loadImage('../sketch/lobol.png');
+    rabbit_image = loadImage('../sketch/coelho.png');
+    board_image = loadImage('../sketch/grama.jpg');
+}
+
+function keyPressed(){
+    if(keyCode === LEFT_ARROW){
+        wolf.x--;
+    }else if(keyCode === RIGHT_ARROW) {
+        wolf.x++;
+    }else if(keyCode === UP_ARROW) {
+        wolf.y--;
+    }else if(keyCode === DOWN_ARROW){
+        wolf.y++;
+    }
+
+    if(keyCode === "A".charCodeAt(0)){
+        rabbit.x--;
+      //  rabbit.y++;
+    }else if(keyCode === "D".charCodeAt(0)) {
+        rabbit.x++;
+      //  rabbit.y--;
+    }else if(keyCode === "W".charCodeAt(0)) {
+        rabbit.y--;
+        //rabbit.x++;
+    }else if(keyCode === "S".charCodeAt(0)) {
+        rabbit.y++;
+       // rabbit.x--;
+    }
+}
+
+function setup() {
+    let size = 100;
+    wolf = new Entity(2, 2, size, wolf_image);
+    rabbit = new Entity(1, 2, size, rabbit_image);
+    board = new Board(4, 6, size, board_image);
+    createCanvas(board.nc * size, board.nl * size);
+}
+
+function draw(){
+    board.draw();
+    wolf.draw();
+    rabbit.draw();
+} 
