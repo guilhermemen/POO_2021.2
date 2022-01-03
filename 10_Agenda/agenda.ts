8b76fd957841af10d6a7b8d876700c0c5fb1bba8
Antonio Guilherme Mendes Sampaio <gmendes.sam@gmail.com>
2022-01-03 17:32:52 -0300

    a

--- 10_Agenda/agenda.ts
+++ 10_Agenda/agenda.ts
@@ -54,6 +54,23 @@ class Contato{
         this.fones.splice(index,1);
     }
 
+    getNome(){
+        return this.nome;
+    }
+    setNome(nome: string){
+        this.nome = nome;
+    }
+
+    getFone(){
+        return this.fones
+    }
+
+    setFone(fone: Fone){
+        for(let i = 0 ; i < this.fones.length; i++){
+            this.add(this.fones[i])
+        }
+    }
+
     public toString() {
         let result =  this.prefix + " " + this.nome + " ";
         for(let i in this.fones){
@@ -64,20 +81,68 @@ class Contato{
     }
 }
 
-let contato2 = new Contato('Mário',[])
-console.log(contato2.toString());
+class agenda{
+    contatos: Array<Contato>
 
-contato2.add(new Fone('tim', '9943355'));
-contato2.add(new Fone('claro', '9124567'));
-contato2.add(new Fone('oi', '3423523'));
+    constructor(){
+        this.contatos = new Array<Contato>();
+    }
 
-console.log(contato2.toString());
+    FindByName(nome: string): number{
+        for(let i = 0; i < this.contatos.length; i++){
+            if(this.contatos[i].getNome() == nome){
+                return i;
+            }
+        }
+        return -1;
+    }
 
-contato2.remove(3);
+    FindCont(nome: string){
+        let contato = this.FindByName(nome);
+        if(contato != 1){
+            return this.contatos[contato];
+        }else{
+            return null;
+        }
+    }
 
-let contato3 = new Contato('Mário',[])
-console.log(contato3.toString());
+    addCont(contato:Contato){
+        let ok = this.FindByName(contato.getNome());
+        if (ok == null) {
+            this.contatos.push(contato);
+            this.contatos.sort((z,x) => z.getNome().localeCompare(x.getNome()))
+        }else{
+            ok.setFone(contato.getFone());
+        }
+    }
+    rmCont(contato:Contato){
+        let Pos = this.FindByName(contato.getNome());
+        if (Pos >- 1){ 
+            this.contatos.splice(Pos, 1);
+            this.contatos.sort((z,x) => z.getNome().localeCompare(x.getNome()));
+        }
+        console.log("este contato foi excluído com sucesso")
+    }
 
-contato3.add(new Fone('tim', '99'));
-contato3.add(new Fone('claro', '91'));
-console.log(contato3.toString());
+    getContato(nome: string){
+        let a = this.FindByName(nome);
+        if (a != -1){
+            return this.contatos[a];
+        }else{
+            return null;
+        }
+    }
+
+    toString(){
+        return this.contatos.join()
+    }
+
+}
+
+let fones = new Fone("claro", "2234")
+let mario = new Contato('Mário', [])
+mario.add(new Fone("vivo", "4566"));
+mario.remove(3);
+
+agenda.add(mario);
+console.log("" + agenda)
