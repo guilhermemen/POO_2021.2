588de99e09ddead77a64814a8c3efcf732dd688d
Antonio Guilherme Mendes Sampaio <gmendes.sam@gmail.com>
2022-01-05 15:34:18 -0300

    a

---
+++ 11_Cinema_Mapas/CinemaMaps.ts
@@ -0,0 +1,62 @@
+class Pessoa{
+    name: string;
+
+    constructor(name: string){
+        this.name = name;
+    }
+}
+
+class Cinema{
+    assentos: Map<number, Pessoa>;
+    nomes: Map<string, number>;
+
+    constructor(public quant: number){
+        this.assentos = new Map<number, Pessoa>();
+        this.nomes = new Map<string, number>();
+    }
+
+    reserva(lugar: number, pessoa: Pessoa){
+        //let ocupado = this.assentos.get(lugar)
+        if (this.assentos.has(lugar)){
+            console.log("assento ocupado");
+            return;
+        }
+        if (this.nomes.has(pessoa.nome)){
+            console.log(" a pesoa ja está no cinema");
+            return;
+        }
+        this.assentos.set(lugar, pessoa);
+        this.nomes.set(pessoa.nome, lugar);
+    }
+
+    //findKey(nome: number): number {
+    //    for (let key, pessoa of this.assentos){
+    //        if (pessoa.nome == nome){
+    //            return key;
+    //        }
+    //    }
+    //    return -1;
+    //}
+
+    cancelar(nome: string){
+        if (!this.nomes.has(nome)){
+            console.log("essa pessoa não foi encontrada");
+            return;
+        }
+        let key = this.nomes.get(nome);
+        this.assentos.delete(key);
+        this.nomes.delete(nome);
+    }
+
+    toString(){
+        for(let i = 0; i < this.quant; i++){
+            let result = "";
+            if (this.assentos.has(i)) {
+                let pessoa =  this.assentos.get(i);
+                result += pessoa.name + " ";
+            }else{
+                result += "- ";
+            }
+        }
+    }
+}
