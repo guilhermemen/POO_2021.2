0888074cfe71490a8c2458b95f6c899791634e32
Antonio Guilherme Mendes Sampaio <gmendes.sam@gmail.com>
2022-02-15 12:35:24 -0300

    atualizando

--- 15_Twitter/twitter.ts
+++ 15_Twitter/twitter.ts
@@ -1,27 +1,56 @@
+class Inbox{
+    timeline: Map<number, Tweet>
+    meusTweets: Map<number, Tweet>;
+
+    constructor(){
+        this.timeline = new Map<number, Tweet>();
+        this.meusTweets = new Map<number, Tweet>();
+    }
+}
+
 class User{
+    inbox: Inbox;
     username: string;
     follower: Map<string, User>;
     following: Map<string, User>;
 
     constructor(username: string){
+        this.inbox = new Inbox();
         this.username = username;
         this.follower = new Map<string, User>();
         this.following = new Map<string, User>();
     }
 
-    toString(){
-        return;
+    seguir(other: User){
+        if(this.username != other.username){
+            if(!this.following.has(other.username)){
+            this.following.set(other.username, other);
+            other.following.set(this.username, this);
+            }
+        }
     }
 
-    follow(other: User){
-       // this.following.push(other);
-        // this.follower.push(other);
+    enviarTweet(tweet: Tweet){
+        this.inbox.timeline(tweet);
+        this.inbox.meusTweets(tweet);
     }
 
-    twitar(other: User){
-        let Tw = new tweet(this.username)
+    dxSeguir(other: string){
+        if(this.following.has(other)){
+            this.following.delete(other);
+                console.log("deixou de seguir esse uuário")
+        }else{
+            console.log("este usuário não existe")
+        }
     }
 
+    getInbox(){
+        return this.inbox;
+    }
+
+    toString(): string {
+        return "usuário: " + this.username ;
+    }
 }
 
 class Controller{
@@ -31,16 +60,31 @@ class Controller{
 class Tweet{
     id: number;
     username: string;
-    msg: string;
-    likes: set<string>;
-    rt: Tweet
+    mensage: string;
+    likes: string [];
+    rt: string [];
 
-    constructor(id: number, username, msg: string){
+    constructor(id: number, username: string, mensage: string,){
         this.id = id;
         this.username = username;
-        this.msg = msg;
-        this.likes = new Set();
-        this.rt = 
+        this.mensage = mensage;
+        this.likes = [];
+        this.rt = [];
+    }
 
+    getId(): number{
+        return this.id;
+    }
+
+    getMensage(): string{
+        return this.mensage;
+    }
+
+    like(){
+        this.likes.push(this.username);
+    }
+
+    getLikes(){
+        return this.likes;
     }
 }
